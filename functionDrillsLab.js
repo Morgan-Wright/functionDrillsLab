0c12aa3355536e7f9664a7305a25f2aa7017cbe8
Morgan-Wright <morgan.q.wright1217@gmail.com>
2022-03-04 20:39:48 -0800

    problem 17 complete

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -293,8 +293,8 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 // let arrayEvaluator = bigOrSmall(bigOrSmallArray)
 
 ////////////////// PROBLEM 16 ////////////////////
-let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
-let loser = 'Glimmer'
+// let contestants = ['Katniss', 'Peeta', 'Fox-face', 'Glimmer', 'Cato', 'Rue', 'Thresh', 'Clove', 'Marvel']
+// let loser = 'Glimmer'
 /*
   Write a function that is called theEliminator, which takes in two arguments, contestants (which will each be an array of strings), and loser (which will be a string).
   The function should loop over the array of contestant names. If the loser string appears in the array, splice it out. Return the new contestants array.
@@ -302,16 +302,16 @@ let loser = 'Glimmer'
 
 //CODE HERE
 
-function theEliminator(contestants, loser){
-  for(let i = 0; i < contestants.length; i++){
-    if(contestants[i] === loser){
-      contestants.splice(i, 1)
-    }
-  }
-  return contestants
-}
+// function theEliminator(contestants, loser){
+//   for(let i = 0; i < contestants.length; i++){
+//     if(contestants[i] === loser){
+//       contestants.splice(i, 1)
+//     }
+//   }
+//   return contestants
+// }
 
-let updatedContestants = theEliminator(contestants, loser)
+// let updatedContestants = theEliminator(contestants, loser)
 
 ////////////////// PROBLEM 17 ////////////////////
 let sampleString = "Hi, my name is Kylo."
@@ -322,6 +322,11 @@ let sampleString = "Hi, my name is Kylo."
 
 //CODE HERE
 
+function makeUppercase(str){
+  console.log(str.toUppercase())
+}
+
+makeUppercase(sampleString)
 
 ////////////////// PROBLEM 18 ////////////////////
 /*
